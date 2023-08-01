4cc794c217d8177e89db47f754ad30074b1d5a6d
Sanjeev Kumar <sanjeev.siderfighter@gmail.com>
2023-08-01 20:21:54 +0530

    config: Add hilt dependency and update compose version

---
+++ app/src/main/java/com/siderfighter/entertainmenttracker/application/EntertainmentTrackerApp.kt
@@ -0,0 +1,8 @@
+package com.siderfighter.entertainmenttracker.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EntertainmentTrackerApp : Application() {
+}
