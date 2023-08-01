8fe688ae85b700e5cdbd2dcb6217f455bf3990ff
Sanjeev Kumar <sanjeev.siderfighter@gmail.com>
2023-08-01 20:25:36 +0530

    config: Create the packages

--- app/src/main/java/com/siderfighter/entertainmenttracker/presentation/MainActivity.kt
+++ app/src/main/java/com/siderfighter/entertainmenttracker/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.siderfighter.entertainmenttracker
+package com.siderfighter.entertainmenttracker.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
---
+++ app/src/main/java/com/siderfighter/entertainmenttracker/utils/Constants.kt
@@ -0,0 +1,3 @@
+package com.siderfighter.entertainmenttracker.utils
+
+const val LOG_TAG = "[EntertainmentTracker]"
