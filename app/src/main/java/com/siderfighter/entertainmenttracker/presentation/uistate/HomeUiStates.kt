5a797288c8f341b1d64b8f3b19f78356811c29ca
Sanjeev Kumar <sanjeev.siderfighter@gmail.com>
2023-08-31 19:36:43 +0530

    :construction: Collect the flow coming from usecase in viewmodel for home screen

--- app/src/main/java/com/siderfighter/entertainmenttracker/presentation/uistate/HomeUiStates.kt
+++ app/src/main/java/com/siderfighter/entertainmenttracker/presentation/uistate/HomeUiStates.kt
@@ -4,7 +4,7 @@ import com.siderfighter.entertainmenttracker.domain.home.entity.CategoryList
 
 sealed interface HomeUiStates {
     data object Loading : HomeUiStates
-    data class Error(val exception: Exception) : HomeUiStates
+    data class Error(val throwable: Throwable) : HomeUiStates
     data object NoData : HomeUiStates
     data class Success(val categoryList: CategoryList) : HomeUiStates
 }
--- app/src/main/java/com/siderfighter/entertainmenttracker/presentation/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/siderfighter/entertainmenttracker/presentation/viewmodel/HomeViewModel.kt
@@ -4,8 +4,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.siderfighter.entertainmenttracker.configs.ICoroutineDispatcherProvider
 import com.siderfighter.entertainmenttracker.domain.home.usecase.GetAllCategoriesUseCase
+import com.siderfighter.entertainmenttracker.presentation.uistate.HomeUiStates
 import com.siderfighter.entertainmenttracker.utils.LOG_TAG
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,10 +21,26 @@ class HomeViewModel @Inject constructor(
     private val dipatcher: ICoroutineDispatcherProvider
 ) : ViewModel() {
 
+    private val mCategoriesFlow = MutableStateFlow<HomeUiStates>(HomeUiStates.Loading)
+    val categoriesFlow = mCategoriesFlow.asStateFlow()
+
     fun getAllCategories() {
         viewModelScope.launch(dipatcher.io) {
-            val allCategories = getAllCategoriesUseCase()
-            println("$LOG_TAG -> categories = $allCategories")
+            getAllCategoriesUseCase()
+                .onStart {
+                    mCategoriesFlow.value = HomeUiStates.Loading
+                }
+                .catch {
+                    mCategoriesFlow.value = HomeUiStates.Error(throwable = it)
+                }
+                .collectLatest { categoriesList ->
+                    mCategoriesFlow.value = if (categoriesList.categories.isEmpty()) {
+                        HomeUiStates.NoData
+                    } else {
+                        HomeUiStates.Success(categoryList = categoriesList)
+                    }
+                    println("$LOG_TAG -> categories = ${categoriesFlow.value}")
+                }
         }
     }
 }
