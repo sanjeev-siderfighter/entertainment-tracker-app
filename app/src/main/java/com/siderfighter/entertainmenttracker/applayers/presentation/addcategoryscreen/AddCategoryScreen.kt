5992b7fd29bb2fb32d475013da2cbbe7476f1317
Sanjeev Kumar <sanjeev.siderfighter@gmail.com>
2023-09-03 09:06:34 +0530

    :construction: Create empty file for add category screen

---
+++ app/src/main/java/com/siderfighter/entertainmenttracker/applayers/presentation/addcategoryscreen/AddCategoryScreen.kt
@@ -0,0 +1,16 @@
+package com.siderfighter.entertainmenttracker.applayers.presentation.addcategoryscreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AddCategoryScreen(modifier: Modifier = Modifier, onConfirmed: () -> Unit) {
+
+}
+
+@Preview
+@Composable
+fun AddCategoryScreenPreview() {
+    AddCategoryScreen {}
+}
