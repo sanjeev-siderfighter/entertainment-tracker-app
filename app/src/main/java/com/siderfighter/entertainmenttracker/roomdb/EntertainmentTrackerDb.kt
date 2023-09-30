58a84e95d2f151d9f4af41bddc38f668e2bd8edf
Sanjeev Kumar <sanjeev.siderfighter@gmail.com>
2023-09-30 18:21:21 +0530

    :card_file_box: Disable exportSchema for local room db

--- app/src/main/java/com/siderfighter/entertainmenttracker/roomdb/EntertainmentTrackerDb.kt
+++ app/src/main/java/com/siderfighter/entertainmenttracker/roomdb/EntertainmentTrackerDb.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.siderfighter.entertainmenttracker.applayers.data.categories.entity.Category
 
-@Database(entities = [Category::class], version = 1)
+@Database(entities = [Category::class], version = 1, exportSchema = false)
 abstract class EntertainmentTrackerDb : RoomDatabase() {
     abstract fun entertainmentTrackerDao(): EntertainmentTrackerDao
 
