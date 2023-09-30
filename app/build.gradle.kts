35a7e71b63e2818be0979d37ffbc6d3f28e7ecd0
Sanjeev Kumar <sanjeev.siderfighter@gmail.com>
2023-09-30 15:48:33 +0530

    :heavy_plus_sign: Add dependencies for compose viewmodel and increase targetSdk and compileSdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.siderfighter.entertainmentprogress"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.siderfighter.entertainmentprogress"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -54,8 +54,8 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.06.00"))
     implementation("androidx.compose.ui:ui")
@@ -69,6 +69,10 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // compose viewmodel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
     // hilt
     implementation("com.google.dagger:hilt-android:2.46")
