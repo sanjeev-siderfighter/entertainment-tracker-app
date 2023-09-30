a6ab7f2ae1668fe4be41b3f6a2fd058d26e8a58c
Sanjeev Kumar <sanjeev.siderfighter@gmail.com>
2023-09-30 18:16:35 +0530

    :arrow_up: Use ksp for hilt (might need to go back to kapt for hilt since it's in alpha) and increase hilt version to 2.48

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    kotlin("kapt")
-    id("com.google.dagger.hilt.android")
+//    kotlin("kapt")
     id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -71,15 +71,18 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // compose lifecycle
-    implementation ("androidx.lifecycle:lifecycle-runtime-compose")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose")
+
+    // compose navigation
+    implementation("androidx.navigation:navigation-compose:2.7.3")
 
     // compose viewmodel
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
     // hilt
-    implementation("com.google.dagger:hilt-android:2.46")
-    kapt("com.google.dagger:hilt-android-compiler:2.46")
+    implementation("com.google.dagger:hilt-android:2.48")
+    ksp("com.google.dagger:hilt-compiler:2.48")
 
     // room db
     implementation("androidx.room:room-runtime:2.5.2")
@@ -106,7 +109,7 @@ dependencies {
 }
 
 // Allow references to generated code
-kapt {
-    correctErrorTypes = true
-    useBuildCache = true
-}
+//kapt {
+//    correctErrorTypes = true
+//    useBuildCache = true
+//}
