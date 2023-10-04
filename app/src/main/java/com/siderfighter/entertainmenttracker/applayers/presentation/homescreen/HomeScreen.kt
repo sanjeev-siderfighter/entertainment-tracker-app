dbb023c3cdffdb0327dc2ceb3b20f158c5f9bdaa
Sanjeev Kumar <sanjeev.siderfighter@gmail.com>
2023-10-04 09:46:05 +0530

    :zap: For the no data state use LaunchedEffect since HomeNoDataScreen contains transition animation. Same approach will be needed for the CategoriesListScreen in future since it will also have transition animation resulting in multiple recompositions

--- app/src/main/java/com/siderfighter/entertainmenttracker/applayers/presentation/homescreen/HomeScreen.kt
+++ app/src/main/java/com/siderfighter/entertainmenttracker/applayers/presentation/homescreen/HomeScreen.kt
@@ -2,15 +2,15 @@ package com.siderfighter.entertainmenttracker.applayers.presentation.homescreen
 
 import android.widget.Toast
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.siderfighter.entertainmenttracker.applayers.presentation.navigator.AppRoutes
+import com.siderfighter.entertainmenttracker.applayers.utils.LOG_TAG
 
 @Composable
 fun HomeScreen(
@@ -20,15 +20,14 @@ fun HomeScreen(
     val context = LocalContext.current
 
     val homeUiState by viewModel.categoriesFlow.collectAsStateWithLifecycle()
-
-    LaunchedEffect(Unit) {
-        viewModel.getAllCategories()
-    }
-    Toast.makeText(context, "$homeUiState", Toast.LENGTH_SHORT).show()
+    println("$LOG_TAG  HomeScreen $homeUiState")
 
     when (homeUiState) {
         is HomeUiState.NoData -> {
-            navController.navigate(AppRoutes.HomeNoDataRoute.route)
+            LaunchedEffect(key1 = homeUiState) { // wrapping in LaunchedEffect since it involves transition animation
+                println("$LOG_TAG when NoData")
+                navController.navigate(AppRoutes.HomeNoDataRoute.route)
+            }
         }
 
         is HomeUiState.Loading -> {
@@ -37,6 +36,12 @@ fun HomeScreen(
 
         else -> {
             Toast.makeText(context, "$homeUiState", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    DisposableEffect(key1 = "HomeScreen") {
+        onDispose {
+            println("$LOG_TAG HomeScreen onDispose")
         }
     }
 }
